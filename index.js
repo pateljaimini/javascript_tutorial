d22201b3151fb79d3a1d0883dcb59a5ad5be40be
jaimini <pateljaimini211@gmail.com>
2023-03-09 20:05:28 +0100

    javascript from beginning

---
+++ index.js
@@ -0,0 +1,11 @@
+document.write('this change by using javascript')
+    // document.write('me')
+    // console API(APPLICATION PROGRAMMING INTERFACE)
+    // ways to write javascript
+    console.log('hello world');
+    console.log('hello world',4+6,"another log");
+    console.warn('this is a warning');
+    console.error('this is a error')
+    console.assert(4==6); 
+    // koi error show karavi 6e log ma 
+    // console.clear();
---
+++ variable.js
@@ -0,0 +1,109 @@
+
+
+// 1. variables
+
+// var number1 = 30;
+// var number2 = 50;
+// console.log(number1 +  number2);
+
+// 2. data types in javascript:-
+
+// 1. string
+// 2. number
+// 3. bigint
+// 4. boolean
+// 5. undefined
+// 6. null
+// 7. symbol
+// 8. object
+ 
+// * object datatypes:-
+// 1. object
+// 2. array
+// 3. date
+
+
+// let a = 'abc';
+// let b=  10;
+// let x = BigInt("123456789012345678901234567890");
+// let c = true;
+// let d;
+// let e = null;
+// let marks = {
+//      jaimini : 90,
+//      yogesh : 90
+// }
+// let f = ['abc','xyz',10];
+// const h = new Date('1996-01-21');
+
+// console.log(a);
+// console.log(b);
+// console.log(x);
+// console.log(c);
+// console.log(typeof(d));
+// console.log(e);
+// console.log(marks);
+// console.log(f);
+// console.log(h);
+
+
+// // concatenating for two different data type:-
+
+// let number = 10;
+// let str = 'jaimini';
+
+// console.log(number + str);
+
+
+// types of operators in javascript:-
+
+// 1. Arithmetic operator:-
+
+// var a = 100;
+var b = 10;
+
+// console.log('the result of a+b is',a+b)
+// console.log('the result of a-b is',a-b)
+// console.log('the result of a*b is',a*b)
+// console.log('the result of a/b is',a/b)
+/*
+2. Assignment operators:-
+ 
+var c =b;
+c+=2;
+c-=2;
+c*=2;
+c/=5;
+
+console.log(c);
+*/
+/*
+3. comparison operators:-
+var x =50;
+var y = 20;
+
+console.log(x==y);
+console.log(x>=y);
+console.log(x<=y);
+console.log(x>y);
+console.log(x<y);
+console.log(x!=y);
+*/
+
+// 4. logical operators:-
+
+// console.log(false&&false);
+// console.log(true&& false);
+// console.log(true && true);
+
+// console.log(false || false);
+// console.log(true || false);
+// console.log(true || true);
+
+
+// console.log(!false);
+// console.log(!true);
+
+
+
+document.write(typeof(16 + 'abc'));
