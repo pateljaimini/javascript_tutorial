f1b8e9fa4df3ded12660c4840d0ddb76b3c908e9
jaimini <pateljaimini211@gmail.com>
2023-04-03 13:22:01 +0200

    adding

---
+++ replacechild & remove child/replace & remove child.js
@@ -0,0 +1,22 @@
+// 1. replacechild:----------
+/*var newElement = document.createElement("li");
+
+var newText = document.createTextNode("Apple");
+
+newElement.appendChild(newText);
+
+var target = document.getElementById("list");
+
+var oldElement = target.children[1];
+
+target.replaceChild(newElement,oldElement);
+
+// parent.replacechild("newvalue","oldvalue")
+// console.log(oldElement);*/
+
+// 2. removechild :----(old value)
+/*var target = document.getElementById("list");
+
+var oldElement = target.children[1];
+
+target.removeChild(oldElement);*/
